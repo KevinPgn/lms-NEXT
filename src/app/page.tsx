e575ae8c907803959195a023189e2f79a00f7de4
TeruBW <kevin.pigeon2810@gmail.com>
2024-09-17 22:11:57 +0200

    responsive

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -6,11 +6,11 @@ import { Button } from "@/components/ui/button"
 
 export default function Home() {
   return (
-  <>
+  <div className="px-3">
     <Headers />
     <main className="h-screen max-w-[1400px] mt-32 text-center text-white mx-auto">
       <h1 className="text-7xl font-semibold bg-clip-text text-transparent bg-gradient-to-r from-white/20 via-white to-white/20 leading-tight">Bringing Communities Together</h1>
-      <p className="text-md text-gray-500 w-[700px] mt-4 mx-auto">Grouple is a vibrant online community platform that empowerspeople to connect, collaborate, and culitivate meaningfulrelationships</p>
+      <p className="text-md text-gray-500 w-[700px] max-md:w-full mt-4 mx-auto">Grouple is a vibrant online community platform that empowerspeople to connect, collaborate, and culitivate meaningfulrelationships</p>
 
       <div className="flex items-center gap-5 justify-center mt-5">
         <button className="flex items-center gap-2 bg-black border border-white/10 px-4 py-2 rounded-md text-white hover:bg-white/10 duration-75">
@@ -25,7 +25,7 @@ export default function Home() {
       </div>
 
       <div className="mt-12 relative">
-      <div className="absolute w-full h-full bg-white/10 -z-10 opacity-50 rounded-full blur-xl"></div>
+      <div className="absolute w-full h-full max-xl:hidden bg-white/10 -z-10 opacity-50 rounded-full blur-xl"></div>
         <Image src="/dashboard-snippet.png" alt="hero" width={1000} height={1000} className="mx-auto" />
       </div>
     </main>
@@ -35,10 +35,10 @@ export default function Home() {
     <section className="mt-40 max-w-[1400px] mx-auto mb-10 relative">
       <div className="absolute w-[500px] h-[200px] bg-white/10 left-1/3 -translate-x-1/2 -top-10 -z-10 opacity-50 rounded-full blur-xl"></div>
       <h2 className="text-center text-4xl font-semibold bg-clip-text text-transparent bg-gradient-to-r from-white/20 via-white to-white/20 leading-tight">Pricing Plans That Fit Your Right</h2>
-      <p className="text-center text-sm text-gray-500 w-[700px] mx-auto mt-1 mb-5">Grouple is a vibrant online community platform that empowerspeople to connect, collaborate, and culitivate meaningfulrelationships</p>
+      <p className="text-center text-sm text-gray-500 w-[700px] max-md:w-full mx-auto mt-1 mb-5">Grouple is a vibrant online community platform that empowerspeople to connect, collaborate, and culitivate meaningfulrelationships</p>
    
     <div className="flex items-center gap-5 justify-center mt-7">
-      <Card className="w-[300px] py-3 bg-black border-white/10 min-h-[300px] text-white">
+      <Card className="w-[300px] max-md:w-full py-3 bg-black border-white/10 min-h-[300px] text-white">
         <CardHeader>
           <CardTitle>99$/month</CardTitle>
           <CardDescription>For small communities</CardDescription>
@@ -58,6 +58,6 @@ export default function Home() {
       </Card>
     </div>
     </section>
-  </>
+  </div>
   );
 }
