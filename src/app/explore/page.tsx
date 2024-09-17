1302d242e2c32d957bce09fb2516633a351312ab
TeruBW <kevin.pigeon2810@gmail.com>
2024-09-17 23:00:45 +0200

    explore

---
+++ src/app/explore/page.tsx
@@ -0,0 +1,12 @@
+import { HeadersExplore } from '@/features/headersExplore/HeadersExplore'
+import React from 'react'
+
+const PageExplore = () => {
+  return (
+    <div>
+        <HeadersExplore />
+    </div>
+  )
+}
+
+export default PageExplore
---
+++ src/features/headersExplore/HeadersExplore.tsx
@@ -0,0 +1,7 @@
+export const HeadersExplore = () => {
+  return <header className="h-16">
+    <nav className="h-full bg-[#080810] shadow-lg flex items-center justify-between px-5">
+        
+    </nav>
+  </header>
+}
