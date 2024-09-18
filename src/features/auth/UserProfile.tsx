3a850a191ea500701cde71771746783d8344e739
TeruBW <kevin.pigeon2810@gmail.com>
2024-09-18 15:28:22 +0200

    user profile

--- src/features/auth/UserProfile.tsx
+++ src/features/auth/UserProfile.tsx
@@ -1,9 +1,29 @@
 "use client"
 import { Avatar, AvatarFallback, AvatarImage } from "@/components/ui/avatar"
 import {DropdownMenu, DropdownMenuContent, DropdownMenuItem, DropdownMenuTrigger} from "@/components/ui/dropdown-menu"
+import { User, LogOut } from "lucide-react"
 
 export const UserProfile = ({session}: {session: any}) => {
   return <>
-  
+    <DropdownMenu>
+      <DropdownMenuTrigger>
+        <Avatar>
+          <AvatarImage src={session.user.image} />
+          <AvatarFallback>
+            {session.user.name?.charAt(0)}
+          </AvatarFallback>
+        </Avatar>
+      </DropdownMenuTrigger>
+      <DropdownMenuContent>
+        <DropdownMenuItem>
+          <User className="w-4 h-4 mr-2" />
+          Profile
+        </DropdownMenuItem>
+        <DropdownMenuItem>
+          <LogOut className="w-4 h-4 mr-2" />
+          Logout
+        </DropdownMenuItem> 
+      </DropdownMenuContent>
+    </DropdownMenu>
   </>
 }
