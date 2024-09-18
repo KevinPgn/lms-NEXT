eb69fea487e1a0191c0a5e7d901b54ed76b4bd2e
TeruBW <kevin.pigeon2810@gmail.com>
2024-09-18 15:19:09 +0200

    dashboard

--- src/features/sidebar/Links.tsx
+++ src/features/sidebar/Links.tsx
@@ -7,14 +7,14 @@ import { cn } from "@/lib/utils"
 export const Links = () => {
   const pathname = usePathname()
   return <div className="flex flex-col gap-2 mt-5">
-    <Link href="/dashboard" className={cn("flex hover:bg-blue-100 duration-75 relative items-center gap-2 px-4 py-4", pathname === "/dashboard" && "bg-blue-100 after:absolute after:right-0 after:top-0 after:w-[5px] after:h-full after:bg-blue-700 after:z-[1] text-blue-700")}>
-      <LayoutDashboard className="w-4 h-4" />
-      <span className="font-medium">Dashboard</span>
+    <Link href="/dashboard" className={cn("flex text-gray-500 hover:bg-blue-100 hover:text-blue-700 duration-75 relative items-center gap-2 px-4 py-4", pathname === "/dashboard" && "bg-blue-100 after:absolute after:right-0 after:top-0 after:w-[5px] after:h-full after:bg-blue-700 after:z-[1] text-blue-700")}>
+      <LayoutDashboard size={22} />
+      <span className="font-medium text-md">Dashboard</span>
     </Link>
 
-    <Link href="/browse" className={cn("flex hover:bg-blue-100 duration-75 relative items-center gap-2 px-4 py-4", pathname === "/browse" && "bg-blue-100 after:absolute after:right-0 after:top-0 after:w-[5px] after:h-full after:bg-blue-700 after:z-[1] text-blue-700")}>
-      <Compass className="w-4 h-4" />
-      <span className="font-medium">Browse</span>
+    <Link href="/browse" className={cn("flex text-gray-500 hover:bg-blue-100 hover:text-blue-700 duration-75 relative items-center gap-2 px-4 py-4", pathname === "/browse" && "bg-blue-100 after:absolute after:right-0 after:top-0 after:w-[5px] after:h-full after:bg-blue-700 after:z-[1] text-blue-700")}>
+      <Compass size={22} />
+      <span className="font-medium text-md">Browse</span>
     </Link>
   </div>
 }
