83d12ec0ddff0a8e40dd61ebfca38b5207d18ebd
TeruBW <kevin.pigeon2810@gmail.com>
2024-09-18 12:29:54 +0200

    groups

--- src/features/headersExplore/Groups.tsx
+++ src/features/headersExplore/Groups.tsx
@@ -61,9 +61,9 @@ export function Groups() {
           <ChevronsUpDown className="ml-2 h-4 w-4 shrink-0 opacity-50" />
         </Button>
       </PopoverTrigger>
-      <PopoverContent className="w-[200px] p-0">
+      <PopoverContent className="w-[100px] p-0">
         <Command>
-          <CommandInput placeholder="Search framework..." />
+          <CommandInput placeholder="Grouple" />
           <CommandList>
             <CommandEmpty>No framework found.</CommandEmpty>
             <CommandGroup>
