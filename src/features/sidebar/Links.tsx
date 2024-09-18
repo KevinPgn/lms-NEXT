5ce3b9839863244e43980349d6fc9b2d54162c9b
TeruBW <kevin.pigeon2810@gmail.com>
2024-09-18 15:19:46 +0200

    link

--- src/features/sidebar/Links.tsx
+++ src/features/sidebar/Links.tsx
@@ -12,7 +12,7 @@ export const Links = () => {
       <span className="font-medium text-md">Dashboard</span>
     </Link>
 
-    <Link href="/browse" className={cn("flex text-gray-500 hover:bg-blue-100 hover:text-blue-700 duration-75 relative items-center gap-2 px-4 py-4", pathname === "/browse" && "bg-blue-100 after:absolute after:right-0 after:top-0 after:w-[5px] after:h-full after:bg-blue-700 after:z-[1] text-blue-700")}>
+    <Link href="/" className={cn("flex text-gray-500 hover:bg-blue-100 hover:text-blue-700 duration-75 relative items-center gap-2 px-4 py-4", pathname === "/" && "bg-blue-100 after:absolute after:right-0 after:top-0 after:w-[5px] after:h-full after:bg-blue-700 after:z-[1] text-blue-700")}>
       <Compass size={22} />
       <span className="font-medium text-md">Browse</span>
     </Link>
