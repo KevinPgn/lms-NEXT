b30c2645d3c99929419249baa7a079d359632852
TeruBW <kevin.pigeon2810@gmail.com>
2024-09-19 12:56:38 +0200

    courses

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -4,7 +4,6 @@ import { getSession } from "@/components/utils/CacheSession";
 import { Categories } from "@/components/categories/Categories";
 import { getCoursesPublished } from "@/server/Courses";
 import { CourseCard } from "@/components/courses/CourseCard";
-import { NoCoursesFound } from "@/components/courses/NoCoursesFound";
 
 export default async function Home() {
   const session = await getSession()
@@ -17,15 +16,11 @@ export default async function Home() {
       <main className="flex flex-col flex-1">
         <HeadersHome session={session} />
         <Categories />
-        {courses.length === 0 ? (
-          <NoCoursesFound />
-        ) : (
-          <div className="flex flex-col gap-5 p-5">
-            {courses.map((course) => (
-              <CourseCard key={course.id} course={course} />
-            ))}
-          </div>
-        )}
+        <div className="flex flex-wrap gap-5 p-5">
+          {courses.map((course: any) => (
+            <CourseCard key={course.id} course={course} />
+          ))}
+        </div>
       </main>
     </div>
   );
--- src/components/courses/CourseCard.tsx
+++ src/components/courses/CourseCard.tsx
@@ -1,5 +1,27 @@
+import { BookOpen } from "lucide-react"
+
 export const CourseCard = ({course}: {course: any}) => {
-  return <>
-    
-  </>
+  return <div className="w-[300px] p-3 rounded-lg border border-gray-200 dark:border-gray-800 cursor-pointer">
+    <div className="w-full h-[150px] bg-pink-400 rounded-lg"></div>
+
+    <h2 className="text-lg font-bold mt-2">{course.title ? course.title : "Course Title"}</h2>
+    <p className="text-sm text-gray-500">{course.description ? course.description : "Course Description"}</p>
+  
+    <div className="flex items-center gap-2 mt-2">
+      <span className="text-xs bg-red-400 text-white px-2 py-1 rounded-md">{course.category ? course.category : "Category"}</span>
+      <span className="text-xs bg-green-400 text-white px-2 py-1 rounded-md">{course.price === 0 ? "Free" : course.price}</span>
+    </div>
+
+    <div className="flex items-center gap-2 mt-5">
+      <div className="flex rounded-full p-1 bg-blue-400">
+        <BookOpen className="w-4 h-4 text-white" />
+      </div>
+      <span className="text-sm text-gray-500">{course._count.lessons} lessons</span>
+    </div>
+
+    {/* Progress bar with number of pourcentage */}
+    <div className="w-full h-[10px] bg-gray-200 dark:bg-gray-800 rounded-full mt-4">
+      <div className="h-full bg-blue-400 rounded-full" style={{width: `${course.progress}%`}}></div>
+    </div>
+  </div>
 }
