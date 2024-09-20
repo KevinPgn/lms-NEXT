eb3a021ab5001685677ee0f19395927dd1640c6c
TeruBW <kevin.pigeon2810@gmail.com>
2024-09-20 12:06:41 +0200

    prisma

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,23 +1,8 @@
-import { HeadersHome } from "@/features/headers/HeadersHome";
-import { SidebarHome } from "@/features/sidebar/SidebarHome";
-import { getSession } from "@/components/utils/CacheSession";
-import { Categories } from "@/components/categories/Categories";
-import { FilteredCourses } from "@/components/courses/FilteredCourses";
-
-export default async function Home({searchParams}: {searchParams: {category: string, search: string}}) {
-  const session = await getSession()
+export default async function Home() {
 
   return (
-    <div className="flex">
-      <SidebarHome />
-
-      <main className="flex flex-col overflow-y-auto flex-1">
-        <HeadersHome session={session} />
-        <Categories />
-        <div className="flex flex-wrap gap-5 p-5">
-          <FilteredCourses category={searchParams.category} search={searchParams.search} />
-        </div>
-      </main>
-    </div>
+    <>
+    
+    </>    
   );
 }
