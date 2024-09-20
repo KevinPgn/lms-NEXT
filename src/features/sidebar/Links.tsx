e15c62937c56e4ebb777d05b04397380efb719a7
TeruBW <kevin.pigeon2810@gmail.com>
2024-09-20 12:33:40 +0200

    courses

--- src/features/sidebar/Links.tsx
+++ src/features/sidebar/Links.tsx
@@ -1,11 +1,39 @@
-import {Sparkles, Monitor, Zap, Users2} from "lucide-react"
+"use client"
+import {Sparkles, CircleArrowUp, Monitor, Zap, Users2} from "lucide-react"
 import Link from "next/link"
+import { usePathname } from "next/navigation"
 
-export const Links = () => {
-  return <div className="flex flex-col gap-2 mt-3">
-    <Link href="/" className="flex items-center gap-2 text-sm text-gray-500 p-2 rounded-md hover:bg-gray-100">
-        <Sparkles className="w-4 h-4" />
-        Dashboard
+export const Links = ({session}: {session: any}) => {
+  const pathname = usePathname()    
+
+  return <div className="flex flex-col gap-2 mt-5 mb-10">
+    <Link href="/" className={`flex items-center gap-2 text-md text-black dark:text-white p-2 px-5 rounded-md hover:bg-gray-100 duration-75 dark:hover:bg-white/10 ${pathname === "/" ? "bg-gray-100 dark:bg-white/10 dark:text-white" : ""}`}>
+        <CircleArrowUp className={`w-5 h-5 text-gray-500 ${pathname === "/" ? "text-black dark:text-white" : ""}`} />
+        Overview
+    </Link>
+
+    <Link href="/course" className={`flex items-center gap-2 text-md text-black dark:text-white p-2 px-5 rounded-md hover:bg-gray-100 duration-75 dark:hover:bg-white/10 ${pathname === "/course" ? "bg-gray-100 dark:bg-white/10 dark:text-white" : ""}`}>
+        <Sparkles className={`w-5 h-5 text-gray-500 ${pathname === "/course" ? "text-black dark:text-white" : ""}`} />
+        Course
+    </Link>
+
+    {session ? (
+    <>
+    <Link href="/mycourses" className={`flex items-center gap-2 text-md text-black dark:text-white p-2 px-5 rounded-md hover:bg-gray-100 duration-75 dark:hover:bg-white/10 ${pathname === "/mycourses" ? "bg-gray-100 dark:bg-white/10 dark:text-white" : ""}`}>
+        <Monitor className={`w-5 h-5 text-gray-500 ${pathname === "/mycourses" ? "text-black dark:text-white" : ""}`} />
+        My Courses
+    </Link>
+
+    <Link href="/learningprogress" className={`flex items-center gap-2 text-md text-black dark:text-white p-2 px-5 rounded-md hover:bg-gray-100 duration-75 dark:hover:bg-white/10 ${pathname === "/learningprogress" ? "bg-gray-100 dark:bg-white/10 dark:text-white" : ""}`}>
+        <Zap className={`w-5 h-5 text-gray-500 ${pathname === "/learningprogress" ? "text-black dark:text-white" : ""}`} />
+        Learning Progress
+    </Link>
+    </>
+    ): null}
+
+    <Link href="/community" className={`flex items-center gap-2 text-md text-black dark:text-white p-2 px-5 rounded-md hover:bg-gray-100 duration-75 dark:hover:bg-white/10 ${pathname === "/community" ? "bg-gray-100 dark:bg-white/10 dark:text-white" : ""}`}>
+        <Users2 className={`w-5 h-5 text-gray-500 ${pathname === "/community" ? "text-black dark:text-white" : ""}`} />
+        Community
     </Link>
   </div>
 }
--- src/features/sidebar/Sidebar.tsx
+++ src/features/sidebar/Sidebar.tsx
@@ -1,14 +1,26 @@
 import Link from "next/link"
 import { Links } from "./Links"
-export const Sidebar = () => {
-  return <div className="w-60 p-2 bg-white shadow-lg border-r border-gray-200 py-3 h-screen sticky top-0">
-    <Link href="/" className="flex items-center gap-2 px-3">
-    <div className="w-9 h-9 rounded-xl bg-black flex items-center justify-center">
-        <div className="w-2 h-2 rounded-full bg-white"></div>
-    </div>
-      <h2 className="text-2xl font-normal">LMSP</h2>
-    </Link>
+import { ModeToggle } from "./DarkMode"
+import { SignInButton } from "../auth/SignInButton"
+import { getSession } from "@/components/utils/CacheSession"
+import { SignOutButton } from "../auth/SignOutButton"
 
-    <Links />
+export const Sidebar = async () => {
+  const session = await getSession()
+
+  return <div className="w-60 p-2 bg-white flex flex-col justify-between dark:bg-black shadow-lg border-r border-gray-200 dark:border-gray-800 py-3 h-screen sticky top-0">
+    <div className="flex flex-col gap-2">
+        <Link href="/" className="flex items-center gap-2 px-3">
+        <div className="w-9 h-9 rounded-xl bg-black dark:bg-white flex items-center justify-center">
+            <div className="w-2 h-2 rounded-full bg-white dark:bg-black"></div>
+        </div>
+        <h2 className="text-2xl font-normal">LMSP</h2>
+        </Link>
+
+        <Links session={session}/>
+        <ModeToggle />
+    </div>
+
+    {session ? <SignOutButton /> : <SignInButton />}
   </div>
 }
