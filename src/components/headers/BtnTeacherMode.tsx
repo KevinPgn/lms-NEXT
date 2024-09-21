b06e78c839e74aa8a2a2a4888963034cea74eb85
TeruBW <kevin.pigeon2810@gmail.com>
2024-09-21 16:18:12 +0200

    user

---
+++ src/components/headers/BtnTeacherMode.tsx
@@ -0,0 +1,9 @@
+"use client"
+import { Button } from "@/components/ui/button"
+export const BtnTeacherMode = () => {
+  return <>
+  <Button variant="ghost" className="text-sm border border-gray-200 dark:border-zinc-800 rounded-md px-3 py-1">
+    Teacher Mode
+  </Button>
+  </>
+}
--- src/components/headers/Headers.tsx
+++ src/components/headers/Headers.tsx
@@ -1,13 +1,23 @@
 "use client"
 import {usePathname} from "next/navigation"
 import { SearchBar } from "./SearchBar"
+import { BtnTeacherMode } from "./BtnTeacherMode"
+import { SignInButton } from "@/features/auth/SignInButton"
+import { UserProfile } from "@/features/auth/UserProfile"
 
 export const Headers = ({session}: {session: any}) => {
   const pathname = usePathname()
 
-  return <header className="flex justify-between items-center p-3 h-20 border-b border-gray-200 dark:border-zinc-800">
-    <nav className="flex items-center gap-2">
+  return <header className="p-3 px-5 h-20 border-b border-gray-200 dark:border-zinc-800">
+    <nav className="flex items-center justify-between gap-2 h-full w-full">
         {pathname === "/" ? <SearchBar /> : null}
+
+        <div className="flex items-center gap-5">
+            <BtnTeacherMode />
+            {!session ? <SignInButton /> : <UserProfile session={session}/>}
+        </div>
     </nav>
+
+
   </header>
 }
