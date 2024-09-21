36733a592f9b9db3beb2b35a19b4224f1c7ee0a9
TeruBW <kevin.pigeon2810@gmail.com>
2024-09-21 21:22:51 +0200

    chapters

---
+++ src/server/Courses.ts
@@ -0,0 +1,113 @@
+"use server"
+import prisma from "@/lib/prisma"
+import {z} from "zod"
+import { authenticatedAction } from "@/lib/safe-actions"
+import { getSession } from "@/components/utils/CacheSession"
+/*
+model Course {
+  id String @id @default(cuid())
+  title String
+  description String
+  image String?
+  price Int
+  published Boolean @default(false)
+  levels String // beginner, intermediate, advanced
+  category String
+
+  authorId String
+  author User @relation(fields: [authorId], references: [id], onDelete: Cascade)
+
+  chapters Chapter[]
+  purchases Purchase[]
+  userProgress UserProgress[]
+
+  createdAt DateTime @default(now())
+  updatedAt DateTime @updatedAt
+}
+
+model Chapter {
+  id String @id @default(cuid())
+  title String
+  content String
+  videoUrl String?
+  freePreview Boolean @default(false)
+
+  courseId String
+  course Course @relation(fields: [courseId], references: [id], onDelete: Cascade)
+
+  userProgress UserProgress[]
+
+  createdAt DateTime @default(now())
+  updatedAt DateTime @updatedAt
+}
+
+model Purchase {
+  id String @id @default(cuid())
+  userId String
+  courseId String
+  course Course @relation(fields: [courseId], references: [id], onDelete: Cascade)
+  user User @relation(fields: [userId], references: [id], onDelete: Cascade)
+
+  createdAt DateTime @default(now())
+  updatedAt DateTime @updatedAt
+
+  @@unique([userId, courseId])
+}
+
+model UserProgress {
+  id String @id @default(cuid())
+  userId String
+  courseId String
+  course Course @relation(fields: [courseId], references: [id], onDelete: Cascade)
+  user User @relation(fields: [userId], references: [id], onDelete: Cascade)
+  isCompleted Boolean @default(false)
+
+  chapterId String
+  chapter Chapter @relation(fields: [chapterId], references: [id], onDelete: Cascade)
+
+  createdAt DateTime @default(now())
+  updatedAt DateTime @updatedAt
+
+  @@unique([userId, courseId, chapterId])
+}
+*/
+
+// Get all courses published
+
+export const getCourses = async (category?: string) => {
+    const session = await getSession()
+    const userId = session?.user?.id
+
+    const courses = await prisma.course.findMany({
+        where: {
+            published: true,
+            category: category
+        },
+        select: {
+            id: true,
+            title: true,
+            description: true,
+            image: true,
+            price: true,
+            levels: true,          
+            purchases: {
+                where: {
+                    userId: userId ?? undefined
+                },
+                select: {
+                   id: true
+                }
+            },
+            _count: {
+                select: {
+                    chapters: true
+                }
+            }
+        }
+    })
+
+    return courses.map((course) => ({
+        ...course,
+        isPurchased: course.purchases.length > 0
+    }))
+}
