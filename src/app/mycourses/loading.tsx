03fa411deb43ae4bbd4404ef4266bb653849dd02
TeruBW <kevin.pigeon2810@gmail.com>
2024-09-21 15:07:04 +0200

    start new lms

--- src/app/mycourses/loading.tsx
+++
@@ -1,5 +0,0 @@
-export default function Loading() {
-    return <div className="w-full h-[500px] flex items-center justify-center">
-      <h1 className="text-2xl font-bold">Loading...</h1>
-    </div>
-  }
--- src/components/courses/NoCoursesFind.tsx
+++
@@ -1,6 +0,0 @@
-export const NoCoursesFind = () => {
-  return <div className="w-full h-[500px] flex items-center justify-center">
-    <h1 className="text-2xl font-bold">No courses found for this category</h1>
-  </div>
-
-}
