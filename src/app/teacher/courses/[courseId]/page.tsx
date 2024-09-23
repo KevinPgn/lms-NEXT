63dbf4b89dfbb47c940b9b2977f2ca1ef83a4bdb
TeruBW <kevin.pigeon2810@gmail.com>
2024-09-23 13:42:32 +0200

    course informations

--- src/app/teacher/courses/[courseId]/page.tsx
+++ src/app/teacher/courses/[courseId]/page.tsx
@@ -3,6 +3,7 @@ import { SidebarTeacher } from '@/features/sidebarTeacher/SidebarTeacher'
 import { Headers } from '@/components/headers/Headers'
 import { getSession } from '@/components/utils/CacheSession'
 import { getCourseById } from '@/server/Courses'
+import {TriangleAlert} from "lucide-react"
 
 interface CoursePageProps {
     params: {
@@ -20,8 +21,12 @@ const CoursePage = async ({params}: CoursePageProps) => {
 
     <main className="flex-1">
       <Headers session={session}/>
-      
-
+      {!course?.data?.published ? (
+        <div className='flex items-center bg-yellow-100 text-yellow-800 p-5 gap-2'>
+          <TriangleAlert className='w-4 h-4' />
+          <span className='text-sm'>This course is unpublished. It will not be visible to the students.</span>
+        </div>
+      ) : null}
     </main>
   </section>  
   )
