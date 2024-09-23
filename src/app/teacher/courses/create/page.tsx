b0870c3e7e105864b0afec92074b9d1d11931de8
TeruBW <kevin.pigeon2810@gmail.com>
2024-09-23 13:15:05 +0200

    create

--- src/app/teacher/courses/create/page.tsx
+++ src/app/teacher/courses/create/page.tsx
@@ -1,8 +1,26 @@
-import React from 'react'
+import { SidebarTeacher } from '@/features/sidebarTeacher/SidebarTeacher'
+import { Headers } from '@/components/headers/Headers'
+import { getSession } from '@/components/utils/CacheSession'
+import { FormCreateCourse } from '@/components/teacherCreateCourse/FormCreateCourse'
 
-const TeacherCourseCreatePage = () => {
+const TeacherCourseCreatePage = async () => {
+    const session = await getSession()
+
   return (
-    <div>TeacherCourseCreatePage</div>
+    <section className="flex">
+      <SidebarTeacher />
+
+      <main className="flex-1">
+        <Headers session={session}/>
+        
+        <div className='max-w-3xl mx-auto mt-10'>
+            <h2 className='text-2xl font-bold'>Name your course</h2>
+            <p className='text-sm text-gray-500'>What would you like to name your course? You can change this later.</p>
+        
+            <FormCreateCourse />
+        </div>
+      </main>
+    </section>  
   )
 }
 
--- src/components/teacherCreateCourse/FormCreateCourse.tsx
+++ src/components/teacherCreateCourse/FormCreateCourse.tsx
@@ -1,5 +1,33 @@
+"use client"
+import { useForm } from "react-hook-form"
+import { createCourse } from "@/server/Courses"
+import { Input } from "@/components/ui/input"
+import { Button } from "@/components/ui/button"
+import { useRouter } from "next/navigation"
 export const FormCreateCourse = () => {
+  const { register, handleSubmit, formState: { errors } } = useForm()
+  const router = useRouter()
+
+  const onSubmit = async (data: any) => {
+    await createCourse(data)
+  }
+
   return <>
-    
+    <form onSubmit={handleSubmit(onSubmit)}>
+        <div className="flex flex-col mt-4">
+            <label htmlFor="title" className="text-sm font-medium">Course title</label>
+            <Input type="text" id="title"
+            {...register("title", {required: true})} placeholder="e.g. 'Mastering Next.js'"
+            />
+            {errors.title && <span className="text-sm text-red-500 mt-2">Title is required</span>}
+            <span className="text-sm text-gray-500 mt-2">Be specific and descriptive with your course title. It helps learners understand what the course is about.</span>
+        </div>
+      <div className="flex mt-8 gap-2">
+        <Button type="button" variant="outline" onClick={() => {
+          router.back()
+        }}>Cancel</Button>
+        <Button variant="default" type="submit">Continue</Button>
+      </div>
+    </form>
   </>
 }
