af025df1502619c26eb59f31928535ffed285824
TeruBW <kevin.pigeon2810@gmail.com>
2024-09-24 17:40:56 +0200

    create new chapters

--- src/components/teacherCourseDetail/ChaptersForm.tsx
+++ src/components/teacherCourseDetail/ChaptersForm.tsx
@@ -1,13 +1,13 @@
 "use client"
 import { Button } from "@/components/ui/button"
-import {Plus} from "lucide-react"
+import {Plus, GripVerticalIcon, PencilLine, Trash} from "lucide-react"
+import { createChapter } from "@/server/Chapters"
 
 export const ChaptersForm = ({courseId, courseChapters}: {courseId: string, courseChapters: any}) => {
-
   return <div className="w-full p-4 bg-[#F4F4F4] dark:bg-transparent dark:border dark:border-gray-700 rounded-md mt-7">
     <div className="flex items-center justify-between">
       <span className="text-lg font-bold">Course Chapters</span>
-      <Button variant="ghost" className="flex items-center gap-2">
+      <Button variant="ghost" className="flex items-center gap-2" onClick={() => createChapter({courseId})}>
         <Plus className="w-4 h-4" />
         Add a Chapter   
       </Button>
@@ -15,8 +15,15 @@ export const ChaptersForm = ({courseId, courseChapters}: {courseId: string, cour
     <div>      
       {courseChapters?.length ? (
         courseChapters.map((chapter: any) => (
-          <div key={chapter.id}>
-            <span>{chapter.title}</span>
+          <div key={chapter.id} className="flex items-center justify-between p-3 bg-[#e3e7ec] dark:bg-gray-800 rounded-md mt-2">
+            <div className="flex items-center gap-2">
+              <GripVerticalIcon size={20} className="cursor-grab" />
+              <span className="text-md font-normal">{chapter.title}</span>
+            </div>
+            <div className="flex items-center gap-5">
+              <PencilLine size={20} className="cursor-pointer" />
+              <Trash size={20} className="cursor-pointer" />
+            </div>
           </div>
         ))
       ) : (
